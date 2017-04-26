1688a5d6058d0f05e8a125bc2d56074606289e20
Alex Wannesian <nkpit14axwa@realgymnasiet.se>
2017-04-26 09:19:47 +0200

    projekt
    projekt meny,

--- index.php
+++ index.php
@@ -32,6 +32,7 @@
             <div class="collapse navbar-collapse" id="myNavbar" style="font-family: myFirstFont;">
                 <ul class="nav navbar-nav navbar-right">
                     <li><a href="index.php">ÅRSKURSER</a></li>
+                    <li><a href="projekt.php">PROJEKT</a></li>
                     <li><a href="amnen.php">ÄMNEN</a></li>
                     <li><a href="citat.php">CITAT</a></li>
                     <li><a href="kontakt.php">KONTAKT</a></li>
